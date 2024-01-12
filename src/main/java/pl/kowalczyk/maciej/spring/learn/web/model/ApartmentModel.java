d8f9c6ca65b48da5c8e60d264763bb30ecc97db1
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-12 13:30:39 +0100

    Added new models

---
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/web/model/ApartmentModel.java
@@ -0,0 +1,49 @@
+package pl.kowalczyk.maciej.spring.learn.web.model;
+
+public class ApartmentModel {
+
+    private Long id;
+    private String name;
+    private Integer price;
+
+    public ApartmentModel() {
+    }
+
+    public ApartmentModel(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ApartmentModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
