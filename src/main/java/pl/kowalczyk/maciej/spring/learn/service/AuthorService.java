c847a3f272ff868eab22172ee1f11d003438bef2
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-14 00:14:58 +0100

    Added service classes

---
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/service/AuthorService.java
@@ -0,0 +1,31 @@
+package pl.kowalczyk.maciej.spring.learn.service;
+
+import org.springframework.stereotype.Service;
+import pl.kowalczyk.maciej.spring.learn.repository.AuthorRepository;
+import pl.kowalczyk.maciej.spring.learn.web.model.AuthorModel;
+
+import java.util.logging.Logger;
+
+@Service
+public class AuthorService {
+
+    private static final Logger LOGGER = Logger.getLogger(AuthorService.class.getName());
+
+    private AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public void list() {
+        LOGGER.info("list()");
+//        authorRepository.list();
+        LOGGER.info("list(...) = ");
+    }
+
+    public void create(AuthorModel authorModel) {
+        LOGGER.info("create(" + authorModel + ")");
+//        authorRepository.create(authorModel);
+        LOGGER.info("create(...) = ");
+    }
+}
