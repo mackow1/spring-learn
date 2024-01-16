7925ffdb51cb2659d80de2aefe1d7dace6e4af26
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-16 12:41:21 +0100

    Modified to use model mapper

--- src/main/java/pl/kowalczyk/maciej/spring/learn/service/ApartmentService.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/service/ApartmentService.java
@@ -1,5 +1,6 @@
 package pl.kowalczyk.maciej.spring.learn.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import pl.kowalczyk.maciej.spring.learn.repository.ApartmentRepository;
 import pl.kowalczyk.maciej.spring.learn.repository.entity.ApartmentEntity;
@@ -13,17 +14,22 @@ public class ApartmentService {
     private static final Logger LOGGER = Logger.getLogger(ApartmentService.class.getName());
 
     private final ApartmentRepository apartmentRepository;
+    private final ModelMapper modelMapper;
 
-    public ApartmentService(ApartmentRepository apartmentRepository) {
+    public ApartmentService(ApartmentRepository apartmentRepository, ModelMapper modelMapper) {
         this.apartmentRepository = apartmentRepository;
+        this.modelMapper = modelMapper;
     }
 
     public ApartmentModel create(ApartmentModel apartmentModel) {
         LOGGER.info("create(" + apartmentModel + ")");
 
-        ApartmentEntity apartmentEntity = convertToEntity(apartmentModel);
+//        ApartmentEntity apartmentEntity = convertToEntity(apartmentModel);
+//        ApartmentEntity savedApartmentEntity = apartmentRepository.save(apartmentEntity);
+//        ApartmentModel convertedApartmentModel = convertToModel(savedApartmentEntity);
+        ApartmentEntity apartmentEntity = modelMapper.map(apartmentModel, ApartmentEntity.class);
         ApartmentEntity savedApartmentEntity = apartmentRepository.save(apartmentEntity);
-        ApartmentModel convertedApartmentModel = convertToModel(savedApartmentEntity);
+        ApartmentModel convertedApartmentModel = modelMapper.map(savedApartmentEntity, ApartmentModel.class);
 
         LOGGER.info("create(...) = " + convertedApartmentModel);
         return convertedApartmentModel;
@@ -37,22 +43,22 @@ public class ApartmentService {
         return null;
     }
 
-    private ApartmentModel convertToModel(ApartmentEntity apartmentEntity) {
-        LOGGER.info("convertToModel(" + apartmentEntity + ")");
+//    private ApartmentModel convertToModel(ApartmentEntity apartmentEntity) {
+//        LOGGER.info("convertToModel(" + apartmentEntity + ")");
+//
+//        ApartmentModel apartmentModel = new ApartmentModel();
+//        apartmentModel.setId(apartmentEntity.getId());
+//        apartmentModel.setName(apartmentEntity.getName());
+//
+//        LOGGER.info("convertToModel(...) = " + apartmentModel);
+//        return apartmentModel;
+//    }
 
-        ApartmentModel apartmentModel = new ApartmentModel();
-        apartmentModel.setId(apartmentEntity.getId());
-        apartmentModel.setName(apartmentEntity.getName());
-
-        LOGGER.info("convertToModel(...) = " + apartmentModel);
-        return apartmentModel;
-    }
-
-    private ApartmentEntity convertToEntity(ApartmentModel apartmentModel) {
-        ApartmentEntity apartmentEntity = new ApartmentEntity();
-        apartmentEntity.setName(apartmentModel.getName());
-        apartmentEntity.setPrice(apartmentModel.getPrice());
-
-        return apartmentEntity;
-    }
+//    private ApartmentEntity convertToEntity(ApartmentModel apartmentModel) {
+//        ApartmentEntity apartmentEntity = new ApartmentEntity();
+//        apartmentEntity.setName(apartmentModel.getName());
+//        apartmentEntity.setPrice(apartmentModel.getPrice());
+//
+//        return apartmentEntity;
+//    }
 }
