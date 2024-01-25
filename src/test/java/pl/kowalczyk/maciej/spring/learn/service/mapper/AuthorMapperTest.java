966e097f20f9581d680bffdb1a05808153c510db
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-25 17:18:22 +0100

    Modified test method

--- src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapperTest.java
+++ src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapperTest.java
@@ -1,12 +1,29 @@
 package pl.kowalczyk.maciej.spring.learn.service.mapper;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import pl.kowalczyk.maciej.spring.learn.repository.entity.AuthorEntity;
+import pl.kowalczyk.maciej.spring.learn.web.model.AuthorModel;
 
 class AuthorMapperTest {
 
+    public static final String AUTHOR_NAME_MACIEJ = "Maciej";
+
     @Test
     void from() {
+        // given
+        AuthorMapper authorMapper = new AuthorMapper();
+        AuthorEntity authorEntity = new AuthorEntity();
+        authorEntity.setFirstName(AUTHOR_NAME_MACIEJ);
+        authorEntity.setLastName("Kowalczyk");
+
+        // when
+        AuthorModel authorModel = authorMapper.from(authorEntity);
+
+        // then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(authorModel, "Model is NULL"),
+                () -> Assertions.assertEquals(AUTHOR_NAME_MACIEJ, authorModel.getFirstName(), "Names are not equal")
+        );
     }
 }
