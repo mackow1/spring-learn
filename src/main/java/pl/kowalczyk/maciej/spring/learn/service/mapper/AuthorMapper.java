aab00cc01b26098b26a99db1312908a504cf386f
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-25 17:05:29 +0100

    Added new test class and method

--- src/main/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapper.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapper.java
@@ -1,6 +1,22 @@
 package pl.kowalczyk.maciej.spring.learn.service.mapper;
 
+import org.modelmapper.ModelMapper;
+import pl.kowalczyk.maciej.spring.learn.repository.entity.AuthorEntity;
+import pl.kowalczyk.maciej.spring.learn.web.model.AuthorModel;
+
+import java.util.logging.Logger;
+
 public class AuthorMapper {
 
+    private static final Logger LOGGER = Logger.getLogger(AuthorMapper.class.getName());
 
+    public AuthorModel from(AuthorEntity authorEntity) {
+        LOGGER.info("from(" + authorEntity + ")");
+
+        ModelMapper modelMapper = new ModelMapper();
+        AuthorModel authorModel = modelMapper.map(authorEntity, AuthorModel.class);
+
+        LOGGER.info("from(...) = " + authorModel);
+        return authorModel;
+    }
 }
---
+++ src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapperTest.java
@@ -0,0 +1,12 @@
+package pl.kowalczyk.maciej.spring.learn.service.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorMapperTest {
+
+    @Test
+    void from() {
+    }
+}
