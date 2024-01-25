3f906088d210d9b9364bddd8cf94bff0a6edb124
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-25 22:05:33 +0100

    Added injection of Mapper and modified method list

--- src/main/java/pl/kowalczyk/maciej/spring/learn/service/AuthorService.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/service/AuthorService.java
@@ -3,6 +3,7 @@ package pl.kowalczyk.maciej.spring.learn.service;
 import org.springframework.stereotype.Service;
 import pl.kowalczyk.maciej.spring.learn.repository.AuthorRepository;
 import pl.kowalczyk.maciej.spring.learn.repository.entity.AuthorEntity;
+import pl.kowalczyk.maciej.spring.learn.service.mapper.AuthorMapper;
 import pl.kowalczyk.maciej.spring.learn.web.model.AuthorModel;
 
 import java.util.List;
@@ -14,16 +15,18 @@ public class AuthorService {
     private static final Logger LOGGER = Logger.getLogger(AuthorService.class.getName());
 
     private AuthorRepository authorRepository;
+    private AuthorMapper authorMapper;
 
-    public AuthorService(AuthorRepository authorRepository) {
+    public AuthorService(AuthorRepository authorRepository, AuthorMapper authorMapper) {
         this.authorRepository = authorRepository;
+        this.authorMapper = authorMapper;
     }
 
     public List<AuthorModel> list() {
         LOGGER.info("list()");
 
         List<AuthorEntity> authorEntities = authorRepository.findAll();
-        List<AuthorModel> authorModels = null;
+        List<AuthorModel> authorModels = authorMapper.fromEntities(authorEntities);
 
         LOGGER.info("list(...) = ");
         return authorModels;
