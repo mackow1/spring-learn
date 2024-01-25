b5b343d924d4d7a66d02dd446ff143e849e18502
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-25 22:03:42 +0100

    Added new test method

--- src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapperSpringTest.java
+++ src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapperSpringTest.java
@@ -1,10 +1,14 @@
 package pl.kowalczyk.maciej.spring.learn.service.mapper;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.kowalczyk.maciej.spring.learn.repository.entity.AuthorEntity;
+import pl.kowalczyk.maciej.spring.learn.web.model.AuthorModel;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class AuthorMapperSpringTest {
@@ -14,5 +18,18 @@ class AuthorMapperSpringTest {
 
     @Test
     void fromEntities() {
+        // given
+        AuthorMapper authorMapper = new AuthorMapper();
+        List<AuthorEntity> authorEntities = new ArrayList<>();
+        authorEntities.add(new AuthorEntity());
+
+        // when
+        List<AuthorModel> authorModels = authorMapper.fromEntities(authorEntities);
+
+        // then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(authorMapper, "List is NULL"),
+                () -> Assertions.assertEquals(authorEntities.size(), authorModels.size(), "Lists are not equal in length")
+        );
     }
 }
