8964a18ec335784cf3341dab6e8034a65c961bf3
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-25 22:02:43 +0100

    Removed method from test class

--- src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapperTest.java
+++ src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/AuthorMapperTest.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 import pl.kowalczyk.maciej.spring.learn.repository.entity.AuthorEntity;
 import pl.kowalczyk.maciej.spring.learn.web.model.AuthorModel;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class AuthorMapperTest {
 
     public static final String AUTHOR_NAME_MACIEJ = "Maciej";
@@ -44,23 +41,6 @@ class AuthorMapperTest {
         Assertions.assertAll(
                 () -> Assertions.assertNotNull(authorEntity, "Entity is NULL"),
                 () -> Assertions.assertEquals(AUTHOR_NAME_MACIEJ, authorEntity.getFirstName(), "Names are not equal")
-        );
-    }
-
-    @Test
-    void givenfromEntities() {
-        // given
-        AuthorMapper authorMapper = new AuthorMapper();
-        List<AuthorEntity> authorEntities = new ArrayList<>();
-        authorEntities.add(new AuthorEntity());
-
-        // when
-        List<AuthorModel> authorModels = authorMapper.fromEntities(authorEntities);
-
-        // then
-        Assertions.assertAll(
-                () -> Assertions.assertNotNull(authorMapper, "List is NULL"),
-                () -> Assertions.assertEquals(authorEntities.size(), authorModels.size(), "Lists are not equal in length")
         );
     }
 }
