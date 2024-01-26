f148409235acb05c617a81029ab1b397927666a8
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-26 13:46:09 +0100

    Added new html with thymeleaf, modified some methods to work with thymeleaf

--- src/main/java/pl/kowalczyk/maciej/spring/learn/web/ApartmentController.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/web/ApartmentController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.kowalczyk.maciej.spring.learn.service.ApartmentService;
@@ -43,15 +44,18 @@ public class ApartmentController {
     }
 
     @GetMapping(value = "/create")
-    public String createView() {
+    public String createView(ModelMap modelMap) {
         LOGGER.info("createView()");
+
+        modelMap.addAttribute("apartmentModel", new ApartmentModel());
+
         LOGGER.info("createView(...) = " + null);
         return "create-apartment.html";
     }
 
     @PostMapping
     public String create(
-            @Valid ApartmentModel apartmentModel, BindingResult bindingResult) {
+            @Valid @ModelAttribute(name = "apartmentModel") ApartmentModel apartmentModel, BindingResult bindingResult) {
         LOGGER.info("create(" + apartmentModel + ")");
 
         if (bindingResult.hasErrors()) {
@@ -63,7 +67,8 @@ public class ApartmentController {
         String result = null;
 
         LOGGER.info("create(...) = " + result);
-        return "apartments.html";
+//        return "apartments.html";
+        return "redirect:/apartments";
     }
 
     public ApartmentModel read(ApartmentModel apartmentModel) {
