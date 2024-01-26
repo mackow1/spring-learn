bf33d900665133f24e4474a28e56ef2487e0d28b
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-26 11:59:01 +0100

    Modified list method and added CarMapper as DI

--- src/main/java/pl/kowalczyk/maciej/spring/learn/service/CarService.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/service/CarService.java
@@ -3,6 +3,7 @@ package pl.kowalczyk.maciej.spring.learn.service;
 import org.springframework.stereotype.Service;
 import pl.kowalczyk.maciej.spring.learn.repository.CarRepository;
 import pl.kowalczyk.maciej.spring.learn.repository.entity.CarEntity;
+import pl.kowalczyk.maciej.spring.learn.service.mapper.CarMapper;
 import pl.kowalczyk.maciej.spring.learn.web.model.CarModel;
 
 import java.util.List;
@@ -14,18 +15,21 @@ public class CarService {
     private static final Logger LOGGER = Logger.getLogger(CarService.class.getName());
 
     private final CarRepository carRepository;
+    private final CarMapper carMapper;
 
-    public CarService(CarRepository carRepository) {
+    public CarService(CarRepository carRepository, CarMapper carMapper) {
         this.carRepository = carRepository;
+        this.carMapper = carMapper;
     }
 
     public List<CarModel> list() {
         LOGGER.info("list()");
 
+        List<CarEntity> carEntities = carRepository.findAll();
+        List<CarModel> carModels = carMapper.fromEntities(carEntities);
 
-
-        LOGGER.info("list(...) = ");
-        return null;
+        LOGGER.info("list(...) = " + carModels);
+        return carModels;
     }
 
     public CarEntity create(CarModel carModel) {
