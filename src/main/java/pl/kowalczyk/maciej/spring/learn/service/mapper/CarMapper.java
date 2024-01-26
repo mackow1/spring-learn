1d179dbbe3ccacbe0f5c50e09457eca65600e6b5
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-26 10:43:38 +0100

    Added new method and test class with method

--- src/main/java/pl/kowalczyk/maciej/spring/learn/service/mapper/CarMapper.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/service/mapper/CarMapper.java
@@ -1,4 +1,20 @@
 package pl.kowalczyk.maciej.spring.learn.service.mapper;
 
+import pl.kowalczyk.maciej.spring.learn.repository.entity.CarEntity;
+import pl.kowalczyk.maciej.spring.learn.web.model.CarModel;
+
+import java.util.logging.Logger;
+
 public class CarMapper {
+
+    private static final Logger LOGGER = Logger.getLogger(CarMapper.class.getName());
+
+    public CarModel from(CarEntity carEntity) {
+        LOGGER.info("from()");
+
+        CarModel result = null;
+
+        LOGGER.info("from(...) = " + result);
+        return result;
+    }
 }
---
+++ src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/CarMapperTest.java
@@ -0,0 +1,30 @@
+package pl.kowalczyk.maciej.spring.learn.service.mapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.kowalczyk.maciej.spring.learn.repository.entity.CarEntity;
+import pl.kowalczyk.maciej.spring.learn.web.model.CarModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarMapperTest {
+
+    public static final String MERCEDES_BENZ_C = "Mercedes-Benz C";
+
+    @Test
+    void from() {
+        // given
+        CarMapper carMapper = new CarMapper();
+        CarEntity carEntity = new CarEntity();
+        carEntity.setModelName(MERCEDES_BENZ_C);
+
+        // when
+        CarModel carModel = carMapper.from(carEntity);
+
+        // then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(carModel, "Model is NULL"),
+                () -> Assertions.assertEquals(MERCEDES_BENZ_C, carModel.getModelName(), "Names are not equal")
+        );
+    }
+}
