88ad1bce0fd00b153cdf9cddbf90a112fba2927d
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-26 10:50:08 +0100

    Modified test method names

--- src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/CarMapperTest.java
+++ src/test/java/pl/kowalczyk/maciej/spring/learn/service/mapper/CarMapperTest.java
@@ -12,7 +12,7 @@ class CarMapperTest {
     public static final String MERCEDES_BENZ_C = "Mercedes-Benz C";
 
     @Test
-    void from() {
+    void givenMapperAndEntity_whenMapperFrom_thenModelIsNotNullAndModelNamesAreEqual() {
         // given
         CarMapper carMapper = new CarMapper();
         CarEntity carEntity = new CarEntity();
@@ -29,7 +29,7 @@ class CarMapperTest {
     }
 
     @Test
-    void testFrom() {
+    void givenMapperAndModel_whenMapperFrom_thenEntityNotNullAndModelNamesAreEqual() {
         // given
 
         // when
