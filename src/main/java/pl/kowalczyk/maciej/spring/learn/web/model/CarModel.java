66126f7d6deabc3cd2da10766dc7441d5b864e39
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-01-30 12:24:01 +0100

    Added Thymeleaf form

--- src/main/java/pl/kowalczyk/maciej/spring/learn/web/model/CarModel.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/web/model/CarModel.java
@@ -2,12 +2,14 @@ package pl.kowalczyk.maciej.spring.learn.web.model;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public class CarModel {
 
     @NotEmpty
     private String modelName;
 
+    @NotNull(message = "pole musi mieć wartość")
     @Min(message = "pole musi mieć wartość minimalną 1", value = 1)
     private Integer horsePower;
 
