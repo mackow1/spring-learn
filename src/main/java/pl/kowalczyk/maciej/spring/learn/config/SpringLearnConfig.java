434f42c36ec01c9c5cf2e958d001e04467b457bf
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-02-01 23:18:47 +0100

    Added configured MessageSource

--- src/main/java/pl/kowalczyk/maciej/spring/learn/config/SpringLearnConfig.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/config/SpringLearnConfig.java
@@ -1,7 +1,9 @@
 package pl.kowalczyk.maciej.spring.learn.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import pl.kowalczyk.maciej.spring.learn.repository.QuoteRepository;
 
 @Configuration
@@ -10,5 +12,13 @@ public class SpringLearnConfig {
     @Bean
     public QuoteRepository quoteRepository() {
         return new QuoteRepository();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:form_error_messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
