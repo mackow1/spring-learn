53a4db442012a51d1e8340c89e13dac50cce67ff
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-02-01 23:50:21 +0100

    Added LocalResolver bean

--- src/main/java/pl/kowalczyk/maciej/spring/learn/config/SpringLearnConfig.java
+++ src/main/java/pl/kowalczyk/maciej/spring/learn/config/SpringLearnConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import pl.kowalczyk.maciej.spring.learn.repository.QuoteRepository;
+
+import java.util.Locale;
 
 @Configuration
 public class SpringLearnConfig {
@@ -20,5 +24,12 @@ public class SpringLearnConfig {
         messageSource.setBasename("classpath:form_error_messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        return localeResolver;
     }
 }
