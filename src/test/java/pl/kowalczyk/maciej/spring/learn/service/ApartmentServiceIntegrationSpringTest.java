0cd4655aa7b49fd8c1d7412030b507c961e8ac1f
Maciej Kowalczyk <mm.kowalczykk@gmail.com>
2024-02-05 19:37:58 +0100

    Added test for method update

--- src/test/java/pl/kowalczyk/maciej/spring/learn/service/ApartmentServiceIntegrationSpringTest.java
+++ src/test/java/pl/kowalczyk/maciej/spring/learn/service/ApartmentServiceIntegrationSpringTest.java
@@ -9,6 +9,7 @@ import pl.kowalczyk.maciej.spring.learn.web.model.ApartmentModel;
 @SpringBootTest
 class ApartmentServiceIntegrationSpringTest {
 
+    public static final String SERVICE_UPDATE_INTEGRATION_TEST_UPDATED_NAME = "ServiceUpdateIntegrationTest-updated";
     @Autowired
     private ApartmentService apartmentService;
 
@@ -25,5 +26,29 @@ class ApartmentServiceIntegrationSpringTest {
 
         // then
         Assertions.assertNotNull(readApartmentModel, "Model is NULL");
+    }
+
+    @Test
+    void update() {
+        // given
+        ApartmentModel apartmentModel = new ApartmentModel();
+        apartmentModel.setName("ServiceUpdateIntegrationTest");
+        apartmentModel.setPrice(1122);
+
+        // when
+        ApartmentModel createdApartmentModel = apartmentService.create(apartmentModel);
+        ApartmentModel readApartmentModel = apartmentService.read(createdApartmentModel.getId());
+
+        readApartmentModel.setName(SERVICE_UPDATE_INTEGRATION_TEST_UPDATED_NAME);
+        readApartmentModel.setPrice(1120);
+
+        ApartmentModel updatedApartmentModel = apartmentService.update(readApartmentModel);
+
+        // then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(updatedApartmentModel, "Model is NULL"),
+                () -> Assertions.assertEquals(createdApartmentModel.getId(), updatedApartmentModel.getId(), "IDs are not equal"),
+                () -> Assertions.assertEquals(SERVICE_UPDATE_INTEGRATION_TEST_UPDATED_NAME, updatedApartmentModel.getName(), "Names are not equal")
+        );
     }
 }
